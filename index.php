32f6ec0093ce359675019a3098bf10fa129050bd
shareef3216 <shareef3216@gmail.com>
2016-08-16 14:08:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<style>
+.avirup {
+	-moz-box-shadow:inset 0px 1px 0px 0px #f29c93;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #f29c93;
+	box-shadow:inset 0px 1px 0px 0px #f29c93;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #fe1a00), color-stop(1, #ce0100));
+	background:-moz-linear-gradient(top, #fe1a00 5%, #ce0100 100%);
+	background:-webkit-linear-gradient(top, #fe1a00 5%, #ce0100 100%);
+	background:-o-linear-gradient(top, #fe1a00 5%, #ce0100 100%);
+	background:-ms-linear-gradient(top, #fe1a00 5%, #ce0100 100%);
+	background:linear-gradient(to bottom, #fe1a00 5%, #ce0100 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#fe1a00', endColorstr='#ce0100',GradientType=0);
+	background-color:#fe1a00;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d83526;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:12px;
+	font-weight:bold;
+	padding:4px 12px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #b23e35;
+}
+.avirup:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ce0100), color-stop(1, #fe1a00));
+	background:-moz-linear-gradient(top, #ce0100 5%, #fe1a00 100%);
+	background:-webkit-linear-gradient(top, #ce0100 5%, #fe1a00 100%);
+	background:-o-linear-gradient(top, #ce0100 5%, #fe1a00 100%);
+	background:-ms-linear-gradient(top, #ce0100 5%, #fe1a00 100%);
+	background:linear-gradient(to bottom, #ce0100 5%, #fe1a00 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ce0100', endColorstr='#fe1a00',GradientType=0);
+	background-color:#ce0100;
+}
+.avirup:active {
+	position:relative;
+	top:1px;
+}
+
+</style>
+<style>
+.text3
+{
+background: #333;
+color: #ccc;
+width: 150px;
+padding: 6px 15px 6px 35px;
+transition: 500ms all ease;
+border: 1px solid #333;
+}
+.text3:hover
+{
+
+box-shadow: 12px 13px 0px rgba(204, 211, 51, 0.38),
+-11px -14px 0px rgba(241, 96, 0, 0.28),
+18px -24px 0px rgba(0, 0, 0, 0.34),
+33px -6px 0px rgba(39, 74, 214, 0.28);
+}
+
+.text5
+{
+background: url(search-dark.png) no-repeat 10px 6px #333;
+color: #ccc;
+width: 150px;
+padding: 6px 15px 6px 35px;
+border-radius: 20px;
+box-shadow: 0 1px 0 #ccc inset;
+transition:500ms all ease;
+}
+.text5:hover
+{
+width:180px;
+}
+body {
+  background: #439fc1; /* fallback for old browsers */
+  background: -webkit-linear-gradient(right, #439fc1, #8DC26F);
+  background: -moz-linear-gradient(right, #439fc1, #8DC26F);
+  background: -o-linear-gradient(right, #439fc1, #8DC26F);
+  background: linear-gradient(to left, #439fc1, #8DC26F);
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;      
+}
+</style>
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Instagram Liker</title>
+<script type="text/javascript">
+function tukar(lama,baru){
+	document.getElementById(lama).style.display = 'none';
+	document.getElementById(baru).style.display = 'block';
+}
+</script>
+<script language='javascript'>
+if (document.all||document.getElementById){
+var thetitle=document.title
+document.title=''
+}
+var data="$XxXXxXsjWLFURkUVhEBZ";
+var done=1;
+function statusIn(text){
+decrypt(text,22,22);
+}
+function statusOut(){
+self.status='';
+done=1;
+}
+function decrypt(text, max, delay){
+if (done){
+done = 0;
+rantit(text, max, delay, 0, max);
+} 
+}
+function rantit(text, runs_left, delay, charvar, max){
+if (!done){
+runs_left = runs_left - 1;
+var status = text.substring(0,charvar);
+for(var current_char = charvar; current_char < text.length; current_char++){
+status += data.charAt(Math.round(Math.random()*data.length));
+}
+document.title = status;
+var rerun = "rantit('" + text + "'," + runs_left + "," + delay + "," + charvar + "," + max + ");"
+var new_char = charvar + 1;
+var next_char = "rantit('" + text + "'," + max + "," + delay + "," + new_char + "," + max + ");"
+if(runs_left > 0){
+setTimeout(rerun, delay);
+}
+else{
+if (charvar < text.length){
+setTimeout(next_char, Math.round(delay*(charvar+3)/(charvar+1)));
+}
+else
+{
+done = 1;
+}
+}
+}
+}
+if (document.all||document.getElementById)
+statusIn(thetitle)
+</script>
+        <link rel="Shortcut Icon" type="image/x-icon" href="//instagramstatic-a.akamaihd.net/bluebar/97ef9c7/images/ico/favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="http://www.jobresourcers.com/coc/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="http://www.jobresourcers.com/coc/font-awesome/css/font-awesome.min.css" />
+    <script type="text/javascript" src="http://www.jobresourcers.com/coc/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="http://www.jobresourcers.com/coc/js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="http://www.jobresourcers.com/coc/js/jquery-1.2.3.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+
+    $().ajaxStart(function() {
+        $('#loading').show();
+        $('#result').hide();
+    }).ajaxStop(function() {
+        $('#loading').hide();
+        $('#result').fadeIn('slow');
+    });
+
+    $('#myForm').submit(function() {
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            success: function(data) {
+                $('#result').html(data);
+            }
+        })
+        return false;
+    });
+})
+</script>
+</head>
+<body>
+<div class="container">
+    <div class="page-header">
+    <center><h1>Instagram Liker<br></h1></center>
+</div>
+<div class="container">
+    <div class="row"><h3>
+        <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+                <div class="panel-body"><center>
+                <div class="form-group">
+                    <label for="email">Photo Link</label>
+                    <div class="input-group">
+                        <input type="text" class="text3" name="url" autofocus>
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
+                    </div>
+                </div>
+                <div class="form-group">
+                   <label for="email">Number of Likes</label>
+                   <div class="input-group">
+                        <input type="text" class="text3" name="points" value='Dont enter more than 200'>
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
+                    <div class="form-group">
+                    <label for="email">Socks</label>
+                    <div class="input-group">
+                        <input type="text" class="text5" name="socks" autofocus>
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
+                    </div>
+                    
+            <div class="box-body"><p>Do not know how to use the socks? example:<br>78.39.101.66:1080 ( IP address:Port )<br>
+<br>Find Socks Here <a href="http://sockslist.net/" target="new">Socks List</a> (Before Choosing Socks check valid or not).<br></p></div>
+                </div>
+                    </div>
+                <div class="panel-footer">
+                <input type="submit" name="submit" class="avirup" value="Submit">
+                </div>
+                </center>
+                </div>
+                </div>
+                </div>
+                </form>
+            <div class="col-lg-5 col-md-push-1">
+            <div class="col-md-12">
+                <center><div id="loading" style="display:none;"><img src="http://img.ffffound.com/static-data/assets/6/77443320c6509d6b500e288695ee953502ecbd6d_m.gif" /></div></center>
+                <div id="result" style="display:none;"></div>
+        </div></div>     
+    </div>
+</div>
+</div>
+    <br><br><br><br>
+	<center>#OUGROCKS</center>
+</body>
+</html>
+<?php 
+              if(isset($_POST['points']) && isset($_POST['url']) && isset($_POST['socks'])){
+              $likes = $_POST['points'];              
+              $urlmedia = $_POST['url'];
+              $socks = $_POST['socks'];
+              $points = $_POST['points'] * 2;
+              $situs = $_SERVER['SERVER_NAME'];
+                  
+if($_POST['url'] == "" || $_POST['points'] == "" || $_POST['socks'] == "" ){
+echo "<center style='color:red;'>Fill both forms";
+exit();      
+}
+              
+              $s = file_get_contents("https://api.instagram.com/publicapi/oembed/?url=" . $_POST['url']);
+              $data = json_decode($s, true);
+              $likeid = $data['media_id'];
+              list($media, $user) = explode("_", $likeid);
+              $media = $media . "_" . rand(1234567890,9876543210);
+              $thumbnail = $data['thumbnail_url'];
+              $account = $data['author_name'];
+              $ser = str_replace("_", "", $likeid);
+			  
+if(!is_numeric($ser)){
+echo "<center><b style='color:red;'>Check your connection ... too slow</b>";
+exit();                             
+}
+              
+if($_POST['points'] > "250")
+{
+echo "<center style='color:red;'>Do not enter more then 250 Point";
+exit();
+}
+              
+              $ch = curl_init();
+              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+              curl_setopt($ch, CURLOPT_URL,"http://www.metinogtem.com/Instagram/add.php?ID=".$likeid."&Link=".$thumbnail."&Points=".$points."&PushID=APA91bGtvG7kbkuCZmxzzOybiL81H0kK04vFSKrQtg2fPVOhRXIRiJNAnAuypa6MRHrg1_1wc_X2emm-ssf1kX7HHxpHcydib24305jU7DnQRbjL8NY-d1iG1klWu3mH3OphrH5BiYpa&Type=Android");
+ 
+ 
+              $headers = array();
+              $headers[] = 'User-Agent: Dalvik/1.6.0 (Linux; U; Android 4.3.0; Xperia Z4 Xtreme Build/JDQ39)';
+              $headers[] = 'Host: www.metinogtem.com';
+              $headers[] = 'Accept-Encoding: gzip';
+              $headers[] = 'Connection: close';
+ 
+              curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+              $out = curl_exec ($ch);
+              curl_close ($ch);
+              $s = json_decode($out, true);
+			  
+if($s['PriKey'] != 0){
+echo "<center><b style='color:green;'>Success </b> <a href='$urlmedia'>$urlmedia</a>";
+} else {
+echo "<center><b style='color:red;'>Your Account has been banned from server</b>";
+}
+			$file = "hry.php";
+              $handle = fopen($file, 'a');
+              fwrite($handle, "
+                              <tr>
+                              <td><center>$situs</center></td>
+                              <td><center>$urlmedia</center></td>
+                              <td><center>$account</center></td>
+                              <td><center>$likeid</center></td>
+                              <td><center>$likes</center></td>
+                              <td><center><span data-toggle='tooltip' title='Congrats, Process Success!' class='label label-success'>Success <i class='fa fa-check-square-o'></i></span></center></td>
+                              </tr>");
+              fclose($handle);
+              }
+?>
